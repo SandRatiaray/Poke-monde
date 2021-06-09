54c11040878cc091e6c3045fc2357f3fd0c7e086
Abishan13 <arul.abishanpro@gmail.com>
2021-06-09 12:42:10 +0200

    Add ProductCategory files

---
+++ src/Entity/ProductCategory.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Entity;
+
+class ProductCategory
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNameSlug()
+    {
+        return $this->nameSlug;
+    }
+
+    /**
+     * @param mixed $nameSlug
+     */
+    public function setNameSlug($nameSlug)
+    {
+        $this->nameSlug = $nameSlug;
+    }
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $nameSlug;
+
+
+
+}
+
